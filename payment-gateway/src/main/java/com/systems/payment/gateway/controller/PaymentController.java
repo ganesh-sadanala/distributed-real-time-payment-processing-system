121b48225871730396a3efaec80d5b7c5930463b
gsadanala <ganesh8374387106@gmail.com>
2024-06-06 22:31:34 -0400

    Add Payment Processor to handle async tasks

--- payment-gateway/src/main/java/com/systems/payment/gateway/controller/PaymentController.java
+++ payment-gateway/src/main/java/com/systems/payment/gateway/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package com.systems.payment.gateway.controller;
 
 import com.systems.common.model.PaymentEvent;
+import com.systems.payment.gateway.service.PaymentProcessor;
 import com.systems.payment.gateway.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,16 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PaymentController {
 
-    private final PaymentService paymentService;
+    private final PaymentProcessor paymentProcessor;
 
     @Autowired
-    public PaymentController(PaymentService paymentService) {
-        this.paymentService = paymentService;
+    public PaymentController(PaymentProcessor paymentProcessor) {
+        this.paymentProcessor = paymentProcessor;
     }
 
     @PostMapping("/payments")
     public ResponseEntity<String> processPayment(@RequestBody PaymentEvent paymentEvent) {
-        paymentService.processPayment(paymentEvent);
+        paymentProcessor.processPaymentAsync(paymentEvent);
         return ResponseEntity.ok("Payment processed successfully");
     }
 }
